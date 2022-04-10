5a293716d5d2ee168e852e8391d5400febcdfc1b
nianjutu <78606982+nianjutu@users.noreply.github.com>
2022-04-11 00:19:27 +0800

    Update hitcarder.py

--- hitcarder.py
+++ hitcarder.py
@@ -125,18 +125,18 @@ class HitCarder(object):
         new_info = def_info.copy()
         new_info.update(magic_code_group)
         # form change
-#         new_info['szgjcs'] = ""
-#         new_info['zgfx14rfhsj'] = ""
-#         new_info['geo_api_info'] = old_info['geo_api_info'] # 定位
-#         new_info['address'] = old_info['address']
-#         new_info['area'] = old_info['area']
-#         new_info['city'] = old_info['city']
-#         new_info['ismoved'] = 0
-#         new_info['sfzx'] = old_info['sfzx'] # 在校
-#         new_info['sfymqjczrj'] = old_info['sfymqjczrj'] # 入境
-#         new_info['sfqrxxss'] = 1 # 属实
-#         new_info['campus'] = "紫金港校区"
-#         new_info['jrdqtlqk[]'] = ""
+        new_info['szgjcs'] = ""
+        new_info['zgfx14rfhsj'] = ""
+        new_info['geo_api_info'] = old_info['geo_api_info'] # 定位
+        new_info['address'] = old_info['address']
+        new_info['area'] = old_info['area']
+        new_info['city'] = old_info['city']
+        new_info['ismoved'] = 0
+        new_info['sfzx'] = old_info['sfzx'] # 在校
+        new_info['sfymqjczrj'] = old_info['sfymqjczrj'] # 入境
+        new_info['sfqrxxss'] = 1 # 属实
+        new_info['campus'] = "紫金港校区"
+        new_info['jrdqtlqk[]'] = 0
 
         self.info = new_info
         # print(json.dumps(self.info))
